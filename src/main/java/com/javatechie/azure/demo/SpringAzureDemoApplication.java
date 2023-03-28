a4401644ba1c20d1793f4e4aaead99a9b5873075
borsztajn1 <loboskop@wp.pl>
2023-03-28 14:04:06 +0200

    Update SpringAzureDemoApplication.java

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats !! !";
+		return "Congrats ! !";
 	}
 
 	public static void main(String[] args) {
